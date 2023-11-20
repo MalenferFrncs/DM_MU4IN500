de3605b46c0f7e6315830fed4be605f6bc55088b
CyranHOEdeBergerac <danael.carbonneau@protonmail.com>
2023-11-20 11:34:09 +0100

    tas min avec un arbre (OK pour la q1)

--- tas_min_tree.ml
+++ tas_min_tree.ml
@@ -1,64 +1,6 @@
 
-
-(*Code caduque à retravailler pour le rééquilibrage du tas tel que défini après*)
-
-let rec descenteReeqRacine (t : 'a tm_tree)  : 'a tm_tree = 
-  match t with 
-  | Empty -> Empty
-  | Leaf (e)-> Leaf(e)
-  | Node (r, fg, fd)-> 
-      (
-        match (fg, fd) with 
-        | (Empty, Empty) -> failwith "Invalid Argument"
-        | (Leaf(e), Empty) -> if (e < r) then Node (e, Leaf(r), Empty) else t
-              
-        | (Leaf(eg), Leaf(ed)) -> 
-            if (eg < r) then 
-              if (eg < ed) then 
-                Node (eg, Leaf(r), Leaf(ed))
-              else
-                Node (ed, Leaf(eg), Leaf(r))
-            else if (ed < r) then 
-              Node (ed, Leaf(eg), Leaf(r))
-            else t
-              
-        | (Node (eg,ffg,ffd), Leaf (ed)) -> 
-            if (eg < r) then 
-              if (eg < ed) then 
-                Node (eg, (descenteReeqRacine (Node(r, ffg,ffd))), fd) 
-              else 
-                Node (ed, fg, Leaf(r))
-            else if (ed < r) then 
-              Node (ed, fg, Leaf(r))
-            else t
-              
-        | (Node (eg,fgfg,fgfd), Node (ed,fdfg,fdfd)) -> 
-            if (eg < r) then 
-              if (eg < ed) then 
-                Node (eg, (descenteReeqRacine (Node(r, fgfg,fgfd))), fd )
-              else 
-                Node (ed, fg, (descenteReeqRacine (Node(r, fdfg,fdfd))))
-            else if (ed < r) then 
-              Node (ed, fg, (descenteReeqRacine (Node(r, fdfg,fdfd))))
-            else t
-        | (_,_) -> failwith "Invalid argument" 
-      )
-
-let supprMin (t : 'a tm_tree) : ('a tm_tree * 'a) = 
-  let (t_sans_dernier, dernier) = remonteValDerniereFeuille t in
-  match t_sans_dernier with 
-  | Empty -> (Empty, dernier)
-  | Leaf (e) -> (Leaf (dernier), e)
-  | Node (r, fg, fd) -> 
-      (*On met la dernière feuille à la place de la racine et on la fait descendre*)
-      let tas_eq = descenteReeqRacine (Node (dernier, fg,fd)) (*Rééquilibrage à faire sur cet arbre*)
-      in (tas_eq, r)
-    
-                                              
-
-
-(*Cette structure est inspirée par celle proposée par Chris Okasaki dans Purely Functionnal Data Structures
-  le rang est défini comme étant la distance vers l'emplacement vide le plus proche,*)
+(*Ce code est inspiré par la structure proposée par Chris Okasaki dans Purely Functionnal Data Structures
+  le rang est défini comme étant la distance vers l'emplacement vide le plus proche*)
 (* Noeud ( rang,ndescendances, elt, fg, fd*)
 type 'a heapTree = E | L of 'a | N of int * int *  'a * 'a heapTree * 'a heapTree;;
 
@@ -67,6 +9,12 @@ let rank (t: 'a heapTree) : int=
   | E -> 0
   | L (_) -> 1
   | N (r,_,_,_,_) -> r ;;
+
+let elt (t : 'a heapTree ) : 'a = 
+  match t with 
+  | E -> failwith "empty heap"
+  | L(e) -> e
+  | N (_,_,e,_,_) -> e
     
 let nbdesc (t : 'a heapTree) : int = 
   match t with 
@@ -75,49 +23,109 @@ let nbdesc (t : 'a heapTree) : int =
   | N(_,nd,_,_,_) -> nd;;
           
 
-
-(*Calcule si le tas a des éléments sur son dernier rang (encore incomplet)*)
 let elemSurDernierRang (h :'a  heapTree) = nbdesc h > (Int.shift_left 1 ((rank h)))-2
                                            
-
+                     
+(*Fonctions auxiliaires servant à faire des modifications locales pour garder la propriété du tas
+  énonçant que la racine est inférieure à la racine de ses fils *) 
+let reeq_tas_droite (t : 'a heapTree) (nfd : 'a heapTree) (res : 'a) : 'a heapTree * 'a = 
+  match t with 
+  | N(r,d, e, fg, fd) -> 
+            (*Garder la propriété du tas*)
+      if e < res then 
+        ( (*On a besoin de rééquilibrer avec l'arbre droite dans le cas où on change de racine du nœud*)
+          match fg with 
+          | E -> (N (( min (rank fg) (rank nfd))+1, d-1, res, fg, nfd),e) 
+          | L(efg) -> 
+              if efg < res then 
+                (N (( min (rank fg) (rank nfd))+1, d-1, efg, L(res), nfd),e)
+              else 
+                (N (( min (rank fg) (rank nfd))+1, d-1, res, fg, nfd),e)
+          |N(rfg,dfg,efg,fgfg,fgfd) -> 
+              if efg < res then 
+                (N (( min (rank fg) (rank nfd))+1, d-1, efg, N(rfg,dfg,res,fgfg,fgfd), nfd),e)
+              else 
+                (N (( min (rank fg) (rank nfd))+1, d-1, res, fg, nfd),e)
+        )
+      else (*res est déjà le minimum dans ce tas, on le renvoie sans modifier le reste*)
+        (N (( min (rank fg) (rank nfd))+1, d-1, e, fg, nfd),res) 
+  | _ -> failwith "Invalid Argument"
+           
+let reeq_tas_gauche (t : 'a heapTree) (nfg : 'a heapTree) (res : 'a) : 'a heapTree * 'a = 
+  match t with 
+  | N(r,d, e, fg, fd) -> 
+  (*Garder la propriété du tas*)
+      if e < res then 
+        ( (*On a besoin de rééquilibrer avec l'arbre droite dans le cas où on change de racine du nœud*)
+          match fd with 
+          | E -> (N (( min (rank nfg) (rank fd))+1, d-1, res, nfg, fd),e) 
+          | L(efd) -> 
+              if efd < res then 
+                (N (( min (rank nfg) (rank fd))+1, d-1, efd, nfg, L(res)),e)
+              else 
+                (N (( min (rank nfg) (rank fd))+1, d-1, res, nfg, fd),e)
+          |N(rfd,dfd,efd,fdfg,fdfd) -> 
+              if efd < res then 
+                (N (( min (rank nfg) (rank fd))+1, d-1, efd, nfg, N(rfd,dfd,res,fdfg,fdfd)),e)
+              else 
+                (N (( min (rank nfg) (rank fd))+1, d-1, res, nfg, fd),e)
+        )
+      else (*res est déjà le minimum dans ce tas, on le renvoie sans modifier le reste*)
+        (N (( min (rank nfg) (rank fd))+1, d-1, e, nfg, fd),res) 
+ (*Fin de garder la propriété du tas*)
+  | _ -> failwith "invalid argument"
+  
+        
 
 let min a b = if a > b  then b else a;;
 
 let rec ajout_tasse (h : 'a heapTree) (x : int) :  'a heapTree = 
   match h with 
   | E -> L(x)
-  | L(e) -> N(1, 1,e, L(x),E) 
-  | N(r,d, e, fg, E) -> N(r+1,d+1, e, fg, L(x))
+  | L(e) -> if e < x then  N(1, 1,e, L(x),E) else N(1, 1,x, L(e),E) 
+  | N(r,d, e, fg, E) -> 
+      if e < x then 
+        N(r+1,d+1, e, fg, L(x))
+      else 
+        N(r+1,d+1, x, fg, L(e)) 
   | N(r, d,e, fg, fd) -> 
       let rfg = (rank fg) and rfd = rank fd in 
       if rfg > rfd then (*On ne peut plus ajouter à gauche sans deséquilibrer*)
-        let nfd = ajout_tasse fd x in N(( min rfg (rank nfd) )+1,d+1,e,fg,nfd)
-      else
-        let nfg = ajout_tasse fg x in N (( min rfd (rank nfg) )+1,d+1, e, nfg, fd) ;;
+        if e < x then 
+          let nfd = ajout_tasse fd x in N(( min rfg (rank nfd) )+1,d+1,e,fg,nfd)
+        else 
+          let nfd = ajout_tasse fd e in N(( min rfg (rank nfd) )+1,d+1,x,fg,nfd) (*x < e, puisque e < elt fg, x < elt fg, la propriété du tas est conservée*)
+      else 
+      if e < x then 
+        let nfg = ajout_tasse fg x in N (( min rfd (rank nfg) )+1,d+1, e, nfg, fd)
+      else 
+        let nfg = ajout_tasse fg e in N (( min rfd (rank nfg) )+1,d+1, x, nfg, fd)
+
+
 
 let rec retrait_tasse (h : 'a heapTree) : 'a heapTree * 'a = 
   match h with 
   | E -> failwith "Empty heap"
   | L(e) -> (E, e)
-  | N(r, d, e, L(efg), E) -> (L( e), efg)
+  | N(r, d, e, L(efg), E) -> 
+      if e < efg then 
+        (L(efg), e)
+      else 
+        (L( e), efg)
   | N(r,d, e, fg, fd) -> 
       if rank fg = rank fd  then 
-        (if (nbdesc fg > nbdesc fd) then 
-           let (nfg, res) = retrait_tasse fg in (N (( min (rank nfg) (rank fd))+1, d-1, e, nfg, fd),res)
-         else
-           let (nfd, res) = retrait_tasse fd in (N (( min (rank nfd) (rank fg))+1, d-1, e, fg, nfd),res) 
-        )
+        if (nbdesc fg > nbdesc fd) then 
+          let (nfg, res) = retrait_tasse fg in (reeq_tas_gauche h nfg res)
+        else
+          let (nfd, res) = retrait_tasse fd in (reeq_tas_droite h nfd res)
+                                               
       else if elemSurDernierRang fd then (*On vérifie s'il y a des choses à retirer à droite avant d'en retirer à gauche !*)
-        let (nfd, res) = retrait_tasse fd in (N (( min (rank nfd) (rank fg))+1, d-1, e, fg, nfd),res) 
+        let (nfd, res) = retrait_tasse fd in (reeq_tas_droite h nfd res) 
       else 
-        let (nfg, res) = retrait_tasse fg in (N (( min (rank nfg) (rank fd))+1, d-1, e, nfg, fd),res)
+        let (nfg, res) = retrait_tasse fg in (reeq_tas_gauche h nfg res);; 
 
 
            
-(*TODO*)
-          (*Still issues after removing 20*)
-;;
-          
 let rec ajout_feuille_iter (l : int list) (h : 'a heapTree) : 'a heapTree = 
   match l with 
   | [] -> h
@@ -126,5 +134,4 @@ let rec ajout_feuille_iter (l : int list) (h : 'a heapTree) : 'a heapTree =
 let rec retrait_feuille_iter (n : int) (h : 'a heapTree) : 'a heapTree = 
   if n = 0 then h else let ( h2, _) = retrait_tasse h in retrait_feuille_iter (n-1) h2;;
           
-let t= ajout_feuille_iter [1;2;3;4;5;6;7;8;9;10;11;12;13;14;15;16;17;18;19;20;21;22;23;24;25;26;27;28;29;30;31;32] E in retrait_feuille_iter 16 t;;
-
+let t= ajout_feuille_iter [16;2;3;11;14;15;10;13;4;5;6;7;8;9;12;1] E in (retrait_feuille_iter 10 t);;
--- tryOCaml/tryocaml.ml
+++ tryOCaml/tryocaml.ml
@@ -2,40 +2,168 @@
 
 (*Ce code est inspiré par la structure proposée par Chris Okasaki dans Purely Functionnal Data Structures
   le rang est défini comme étant la distance vers l'emplacement vide le plus proche*)
-
-type 'a heapTree = E | N of int * 'a * 'a heapTree * 'a heapTree;;
+(* Noeud ( rang,ndescendances, elt, fg, fd*)
+type 'a heapTree = E | L of 'a | N of int * int *  'a * 'a heapTree * 'a heapTree;;
 
 let rank (t: 'a heapTree) : int=
   match t with
   | E -> 0
-  | N (r,_,_,_) -> r
+  | L (_) -> 1
+  | N (r,_,_,_,_) -> r ;;
+
+let elt (t : 'a heapTree ) : 'a = 
+  match t with 
+  | E -> failwith "empty heap"
+  | L(e) -> e
+  | N (_,_,e,_,_) -> e
+    
+let nbdesc (t : 'a heapTree) : int = 
+  match t with 
+  |  E -> 0
+  | L(_) -> 0
+  | N(_,nd,_,_,_) -> nd;;
           
-      (*Construit un tas en gardant la propriété sur le rang et le tassage à gauche*)
 
 
-let min a b = if a > b  then b else a
+
+let elemSurDernierRang (h :'a  heapTree) = nbdesc h > (Int.shift_left 1 ((rank h)))-2
+                                           
+                                                      
+
+
+let min a b = if a > b  then b else a;;
 
 let rec ajout_tasse (h : 'a heapTree) (x : int) :  'a heapTree = 
   match h with 
-  | E -> N (1, x, E,E)
-  | N(r, e, E, E) -> N(r, e, N(1,x,E,E),E) 
-  | N(r, e, fg, E) -> N(r+1, e, fg, N(1,x,E,E))
-  | N(r, e, fg, fd) -> 
+  | E -> L(x)
+  | L(e) -> if e < x then  N(1, 1,e, L(x),E) else N(1, 1,x, L(e),E) 
+  | N(r,d, e, fg, E) -> 
+      if e < x then 
+        N(r+1,d+1, e, fg, L(x))
+      else 
+        N(r+1,d+1, x, fg, L(e)) 
+  | N(r, d,e, fg, fd) -> 
       let rfg = (rank fg) and rfd = rank fd in 
       if rfg > rfd then (*On ne peut plus ajouter à gauche sans deséquilibrer*)
-        let nfd = ajout_tasse fd x in N(( min rfg (rank nfd) )+1,e,fg,nfd)
-      else
-        let nfg = ajout_tasse fg x in N (( min rfd (rank nfg) )+1, e, nfg, fd) ;;
+        if e < x then 
+          let nfd = ajout_tasse fd x in N(( min rfg (rank nfd) )+1,d+1,e,fg,nfd)
+        else 
+          let nfd = ajout_tasse fd e in N(( min rfg (rank nfd) )+1,d+1,x,fg,nfd) (*x < e, puisque e < elt fg, x < elt fg, la propriété du tas est conservée*)
+      else 
+      if e < x then 
+        let nfg = ajout_tasse fg x in N (( min rfd (rank nfg) )+1,d+1, e, nfg, fd)
+      else 
+        let nfg = ajout_tasse fg e in N (( min rfd (rank nfg) )+1,d+1, x, nfg, fd)
+
+
 
 let rec retrait_tasse (h : 'a heapTree) : 'a heapTree * 'a = 
   match h with 
   | E -> failwith "Empty heap"
-  | N(r, e, E, E) -> (E, e)
-  | N(r, e, N(_,efg,E,E), E) -> (N (r-1, e, E,E), efg)
-  | N(r, e, N(_,efg,E,E), N(_,efd,E,E)) -> (N (r-1, e, fg,E), efd)                             
-  | N(r, e, fg, fd) -> _ (*TODO*)
-;;
+  | L(e) -> (E, e)
+  | N(r, d, e, L(efg), E) -> 
+      if e < efg then 
+        (L(efg), e)
+      else 
+        (L( e), efg)
+  | N(r,d, e, fg, fd) -> 
+      if rank fg = rank fd  then 
+        if (nbdesc fg > nbdesc fd) then 
+          let (nfg, res) = retrait_tasse fg in (
+            (*Garder la propriété du tas*)
+            if e < res then 
+              ( (*On a besoin de rééquilibrer avec l'arbre droite dans le cas où on change de racine du nœud*)
+                match fd with 
+                | E -> (N (( min (rank nfg) (rank fd))+1, d-1, res, nfg, fd),e) 
+                | L(efd) -> 
+                    if efd < res then 
+                      (N (( min (rank nfg) (rank fd))+1, d-1, efd, nfg, L(res)),e)
+                    else 
+                      (N (( min (rank nfg) (rank fd))+1, d-1, res, nfg, fd),e)
+                |N(rfd,dfd,efd,fdfg,fdfd) -> 
+                    if efd < res then 
+                      (N (( min (rank nfg) (rank fd))+1, d-1, efd, nfg, N(rfd,dfd,res,fdfg,fdfd)),e)
+                    else 
+                      (N (( min (rank nfg) (rank fd))+1, d-1, res, nfg, fd),e)
+              )
+            else (*res est déjà le minimum dans ce tas, on le renvoie sans modifier le reste*)
+              (N (( min (rank nfg) (rank fd))+1, d-1, e, nfg, fd),res) 
+          ) (*Fin de garder la propriété du tas*)
+        else
+          let (nfd, res) = retrait_tasse fd in 
           
+          (
+            (*Garder la propriété du tas*)
+            if e < res then 
+              ( (*On a besoin de rééquilibrer avec l'arbre droite dans le cas où on change de racine du nœud*)
+                match fg with 
+                | E -> (N (( min (rank fg) (rank nfd))+1, d-1, res, fg, nfd),e) 
+                | L(efg) -> 
+                    if efg < res then 
+                      (N (( min (rank fg) (rank nfd))+1, d-1, efg, L(res), nfd),e)
+                    else 
+                      (N (( min (rank fg) (rank nfd))+1, d-1, res, fg, nfd),e)
+                |N(rfg,dfg,efg,fgfg,fgfd) -> 
+                    if efg < res then 
+                      (N (( min (rank fg) (rank nfd))+1, d-1, efg, N(rfg,dfg,res,fgfg,fgfd), nfd),e)
+                    else 
+                      (N (( min (rank fg) (rank nfd))+1, d-1, res, fg, nfd),e)
+              )
+            else (*res est déjà le minimum dans ce tas, on le renvoie sans modifier le reste*)
+              (N (( min (rank fg) (rank nfd))+1, d-1, e, fg, nfd),res) 
+          ) (*Fin de garder la propriété du tas*)
+                                                
+      else if elemSurDernierRang fd then (*On vérifie s'il y a des choses à retirer à droite avant d'en retirer à gauche !*)
+        let (nfd, res) = retrait_tasse fd in 
+        (
+            (*Garder la propriété du tas*)
+          if e < res then 
+            ( (*On a besoin de rééquilibrer avec l'arbre droite dans le cas où on change de racine du nœud*)
+              match fg with 
+              | E -> (N (( min (rank fg) (rank nfd))+1, d-1, res, fg, nfd),e) 
+              | L(efg) -> 
+                  if efg < res then 
+                    (N (( min (rank fg) (rank nfd))+1, d-1, efg, L(res), nfd),e)
+                  else 
+                    (N (( min (rank fg) (rank nfd))+1, d-1, res, fg, nfd),e)
+              |N(rfg,dfg,efg,fgfg,fgfd) -> 
+                  if efg < res then 
+                    (N (( min (rank fg) (rank nfd))+1, d-1, efg, N(rfg,dfg,res,fgfg,fgfd), nfd),e)
+                  else 
+                    (N (( min (rank fg) (rank nfd))+1, d-1, res, fg, nfd),e)
+            )
+          else (*res est déjà le minimum dans ce tas, on le renvoie sans modifier le reste*)
+            (N (( min (rank fg) (rank nfd))+1, d-1, e, fg, nfd),res) 
+        ) (*Fin de garder la propriété du tas*)
+        
+      else 
+        let (nfg, res) = retrait_tasse fg in
+        (
+        (*Garder la propriété du tas*)
+          if e < res then 
+            ( (*On a besoin de rééquilibrer avec l'arbre droite dans le cas où on change de racine du nœud*)
+              match fd with 
+              | E -> (N (( min (rank nfg) (rank fd))+1, d-1, res, nfg, fd),e) 
+              | L(efd) -> 
+                  if efd < res then 
+                    (N (( min (rank nfg) (rank fd))+1, d-1, efd, nfg, L(res)),e)
+                  else 
+                    (N (( min (rank nfg) (rank fd))+1, d-1, res, nfg, fd),e)
+              |N(rfd,dfd,efd,fdfg,fdfd) -> 
+                  if efd < res then 
+                    (N (( min (rank nfg) (rank fd))+1, d-1, efd, nfg, N(rfd,dfd,res,fdfg,fdfd)),e)
+                  else 
+                    (N (( min (rank nfg) (rank fd))+1, d-1, res, nfg, fd),e)
+            )
+          else (*res est déjà le minimum dans ce tas, on le renvoie sans modifier le reste*)
+            (N (( min (rank nfg) (rank fd))+1, d-1, e, nfg, fd),res) 
+        ) (*Fin de garder la propriété du tas*);; 
+
+
+           
+(*TODO*)
+          (*Still issues after removing 20*)
+
 let rec ajout_feuille_iter (l : int list) (h : 'a heapTree) : 'a heapTree = 
   match l with 
   | [] -> h
@@ -44,29 +172,25 @@ let rec ajout_feuille_iter (l : int list) (h : 'a heapTree) : 'a heapTree =
 let rec retrait_feuille_iter (n : int) (h : 'a heapTree) : 'a heapTree = 
   if n = 0 then h else let ( h2, _) = retrait_tasse h in retrait_feuille_iter (n-1) h2;;
           
-ajout_feuille_iter [1;2;3;4;5;6;7;9;10;11;12;13;14;15;16;17;18;19;20] E
-    
-  
-  (N (-3, 1,
-      N (-2, 2,
-         N (3, 4, 
-            N (2, 9, 
-               N (1, 17, E, E), 
-               N (1, 18, E, E)),
-            N (2, 10, 
-               N (1, 19, E, E), 
-               N (1, 20, E, E))),
-         N (-1, 5, 
-            E, 
-            N (1, 12, E, E))),
-      N (3, 3, 
-         N (2, 6, 
-            N (1, 13, E, E), 
-            N (1, 14, E, E)),
-         N (2, 7, 
-            N (1, 15, E, E), 
-            N (1, 16, E, E)))),
-   11)
+let t= ajout_feuille_iter [16;2;3;11;14;15;10;13;4;5;6;7;8;9;12;1] E in (t,retrait_tasse t);;
+
+
+N (4, 15, 1,
+   N (3, 7, 2, 
+      N (2, 3, 4, 
+         N (1, 1, 11, 
+            L 16, E), 
+         L 13),
+      N (2, 2, 5, 
+         L 14,
+         L 6)),
+   N (3, 6, 3, 
+      N (2, 2, 7, 
+         L 15, 
+         L 8), 
+      N (2, 2, 9, 
+         L 10, 
+         L 12)))
 
 
 
