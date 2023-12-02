e897ffcfdb363798608edce870672257acb449f0
CyranHOEdeBergerac <danael.carbonneau@protonmail.com>
2023-12-03 00:04:00 +0100

    utilitaires

--- au propre/arbres_dot.sh
+++ au propre/arbres_dot.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-    if (make > trace_compilation.txt) then 
+    if (ocamlopt int128.ml tas_min_arbre.ml tas_min_tab.ml file_binomiale.ml manipulation_fichiers.ml test_graphique.ml -o test_graphique.exe) then 
 
     echo "La compilation a reussi ! On peut lancer l'exécution"
 
-    ./test.exe
+    ./test_graphique.exe
 
 
-    dot -Tpng tas_tab.dot -o Images/tas_tab.png
-    dot -Tpng tas_arbre.dot -o Images/tas_arbre.png
+    dot -Tpng tas_tab_cons.dot -o Images/tas_tab_cons.png
+    dot -Tpng tas_tab_ajouts.dot -o Images/tas_tab_ajouts.png
+    dot -Tpng tas_tab_ajouts_supprMin.dot -o Images/tas_tab_ajouts_supprMin.png
 
+    dot -Tpng tas_arbre_cons.dot -o Images/tas_arbre_cons.png
+    dot -Tpng tas_arbre_ajouts.dot -o Images/tas_arbre_ajouts.png
+    dot -Tpng tas_arbre_ajouts_supprMin.dot -o Images/tas_arbre_ajouts_supprMin.png
+
+
+    make clean
     else 
         echo "Attention ! Vous n'avez peut-être pas la bonne version de OCaml, il faut utiliser la version 4.14."
     fi
