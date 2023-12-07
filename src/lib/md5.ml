00bff109c87eddf8761dcc95329501396711e423
Malenferfrncs <francois.malenfer@gmail.com>
2023-12-07 07:53:57 +0100

    ladies and gentlemen we got him

--- src/lib/md5.ml
+++ src/lib/md5.ml
@@ -1,5 +1,180 @@
-open Int32
-open Int128
+open Int32;;
+open Int64;;
+open Int128 ;;
+
+
+
+
+    
+
+    
+let hex_to_chr (hex : string ) : char = Char.chr (Int32.to_int (Int32.of_string hex))
+    (*bon*)
+
+let str_of_int64 (bigmot : int64): string=
+  let bit_faible_64 (t : int64) : string =
+  
+    let bit_n_is_1 (entier : int64) (n:int): bool =
+      (Int64.logand (Int64.shift_right entier n) 1L) = 1L
+    in
+  
+    if (bit_n_is_1 t 3)
+    then if (bit_n_is_1 t 2)
+      then if (bit_n_is_1 t 1)
+        then if (bit_n_is_1 t 0)
+          then "f"
+          else "e"
+        else if (bit_n_is_1 t 0)
+        then "d"
+        else "c"
+      else if (bit_n_is_1 t 1)
+      then if (bit_n_is_1 t 0)
+        then "b"
+        else "a"
+      else if (bit_n_is_1 t 0)
+      then "9"
+      else "8"
+    else if (bit_n_is_1 t 2)
+    then if (bit_n_is_1 t 1)
+      then if (bit_n_is_1 t 0)
+        then "7"
+        else "6"
+      else if (bit_n_is_1 t 0)
+      then "5"
+      else "4"
+    else if (bit_n_is_1 t 1)
+    then if (bit_n_is_1 t 0)
+      then "3"
+      else "2"
+    else if (bit_n_is_1 t 0)
+    then "1"
+    else "0"
+  in
+  let rec str_byte_of_int64 (mot : int64) (res : string) (i:int): string =
+    if i = 8 then res
+    else 
+      let p = (bit_faible_64 mot) in
+      let g = (bit_faible_64 (Int64.shift_right mot 4)) in
+      let str = String.make 1 (hex_to_chr ("0x"^g^p)) in
+      str_byte_of_int64 (Int64.shift_right_logical mot 8) (str^res) (i+1)
+  in
+  str_byte_of_int64 bigmot "" 0
+;;
+  
+
+let str_of_int32 (mot : int32 ) : string = 
+  let bit_faible_32 (t : int32) : string =
+  
+    let bit_n_is_1 (entier : int32) (n:int): bool =
+      (Int32.logand (Int32.shift_right entier n) 1l) = 1l
+    in
+  
+    if (bit_n_is_1 t 3)
+    then if (bit_n_is_1 t 2)
+      then if (bit_n_is_1 t 1)
+        then if (bit_n_is_1 t 0)
+          then "f"
+          else "e"
+        else if (bit_n_is_1 t 0)
+        then "d"
+        else "c"
+      else if (bit_n_is_1 t 1)
+      then if (bit_n_is_1 t 0)
+        then "b"
+        else "a"
+      else if (bit_n_is_1 t 0)
+      then "9"
+      else "8"
+    else if (bit_n_is_1 t 2)
+    then if (bit_n_is_1 t 1)
+      then if (bit_n_is_1 t 0)
+        then "7"
+        else "6"
+      else if (bit_n_is_1 t 0)
+      then "5"
+      else "4"
+    else if (bit_n_is_1 t 1)
+    then if (bit_n_is_1 t 0)
+      then "3"
+      else "2"
+    else if (bit_n_is_1 t 0)
+    then "1"
+    else "0"   
+  in
+  (*bon*)
+  let rec str_byte_of_int32 (mot : int32) (res : string) (i:int): string =
+    if i = 4 then res
+    else 
+      let p = (bit_faible_32 mot) in
+      let g = (bit_faible_32 (Int32.shift_right mot 4)) in
+      let str = String.make 1 (hex_to_chr ("0x"^g^p)) in
+      str_byte_of_int32 (Int32.shift_right_logical mot 8) (str^res) (i+1)
+  in
+  str_byte_of_int32 mot "" 0
+;;
+
+
+  
+
+let padding (str : string) (len : int) (len_total : int64 ) (fin :bool ) : string = 
+  let remplire (str : string) (taille : string) (i : int) :char =
+    if i < (String.length str) then str.[i]
+    else if (i=(String.length str) && fin) then (Char.chr 128) 
+    else if i >= 56 then taille.[(7-(i mod 56))]
+    else '\000'
+  in
+  (* bon *)
+  let taille_str = str_of_int64 len_total in
+  String.init 64 (remplire str taille_str) 
+  
+  
+                 
+  
+;;  
+
+
+
+let rec int32_array_of_str (str :string) (len_total : int): int32 array list = 
+  
+  let len = String.length str in
+  if len < 56 then  
+    let pad_str = padding  str len (Int64.mul (Int64.of_int (len_total+len)) 8L) true in 
+    
+    let res = Array.make 16 0l in
+    for i = 0 to 15 do 
+      let res_i32 = String.get_int32_le pad_str (i*4) in
+      
+      res.(i)<- res_i32
+    done ; 
+    res::[] 
+         
+         
+  else if len >= 64 then
+    let res = Array.make 16 0l in
+    for i = 0 to 15 do 
+      let res_i32 = String.get_int32_le str (i*4) in
+      
+      res.(i)<- res_i32
+    done ;
+    res::(int32_array_of_str (String.sub str 64 (len-64)) (len_total + 64))
+         
+         
+  else 
+    let debut_res = str^(String.make 1 (Char.chr 128))^(String.make (64-len-1) '\000') in
+    let fin_res = (padding "" 0  (Int64.mul (Int64.of_int (len_total+len)) 8L)) false in
+    let res_d = Array.make 16 0l in
+    let res_f = Array.make 16 0l in
+    for i = 0 to 15 do 
+      let res_i32_d = String.get_int32_le debut_res (i*4) in
+      let res_i32_f = String.get_int32_le fin_res (i*4) in
+      res_d.(i)<- res_i32_d;
+      res_f.(i)<- res_i32_f;
+    done ;
+    res_d::res_f::[]
+    
+    
+;;
+
   
 let rol32 a n = Int32.logor (Int32.shift_left a n)  (Int32.shift_right_logical a (32 - n))
                                            
@@ -7,9 +182,17 @@ let rol32 a n = Int32.logor (Int32.shift_left a n)  (Int32.shift_right_logical a
                               
 let f1 (b : int32) (c : int32) (d : int32) : int32 = Int32.logor (Int32.logand b c) (Int32.logand (Int32.lognot b) d)
 let f2 (b : int32) (c : int32) (d : int32) : int32 = Int32.logor (Int32.logand b d) (Int32.logand c (Int32.lognot d) )
-let f3 (b : int32) (c : int32) (d : int32) : int32  = Int32.logxor b  (Int32.logxor c d)
+let f3 (b : int32) (c : int32) (d : int32) : int32  = Int32.logxor d  (Int32.logxor c b)
 let f4 (b : int32) (c : int32) (d : int32) : int32 = Int32.logxor c ( Int32.logor b (Int32.lognot d))
     
+let finish (entre : int32) : int32 =
+  let res = 0l in
+  let res = Int32.logor res (Int32.shift_left (Int32.logand entre 0x000000FFl) 24) in 
+  let res = Int32.logor res (Int32.shift_left (Int32.logand entre 0x0000FF00l) 8) in 
+  let res = Int32.logor res (Int32.shift_right_logical (Int32.logand entre 0x00FF0000l) 8) in 
+  let res = Int32.logor res (Int32.shift_right_logical (Int32.logand entre 0xFF000000l) 24) in 
+  res
+;;
 
 
 let s = [| 7; 12; 17; 22;  7; 12; 17; 22;  7; 12; 17; 22;  7; 12; 17; 22; 
@@ -33,57 +216,91 @@ let k = [| 0xd76aa478l; 0xe8c7b756l; 0x242070dbl; 0xc1bdceeel ;
            0x655b59c3l; 0x8f0ccc92l; 0xffeff47dl; 0x85845dd1l ;
            0x6fa87e4fl; 0xfe2ce6e0l; 0xa3014314l; 0x4e0811a1l ;
            0xf7537e82l; 0xbd3af235l; 0x2ad7d2bbl; 0xeb86d391l |]
+  
         
-        
-
-let h = [| 0x67452301l; 0xefcdab89l; 0x98badcfel; 0x10325476l |];;
-
-
-
-let cycle_512 (m : int32 array) : Int128.t =
+let digest (str:string) : Int128.t =        
   
-  let a, b, c, d = ref h.(0), ref h.(1), ref h.(2), ref h.(3) in
-  let r :int32 ref = ref 0l in
-  let g :int ref = ref 0 in
+  let m_li : int32 array list = int32_array_of_str str 0 in
 
-  for i = 0 to 63 do
-    
-    
-    if   i  < 16 then (
-      r := f1 !b !c !d ;
-      g := i ; )
+  let h = [| 0x67452301l; 0xefcdab89l; 0x98badcfel; 0x10325476l |] in
+
+
+  let md5 (m : int32 array) : unit =
   
-    else if i < 32 then (
-      r := f2 !b !c !d ;
-      g := (5*i + 1) mod 16 ; )
+    let a, b, c, d = ref h.(0), ref h.(1), ref h.(2), ref h.(3) in
+    let r :int32 ref = ref 0l in
+    let g :int ref = ref 0 in
+
+    for i = 0 to 63 do 
+  
+      if   i  < 16 then (
+        r := f1 !b !c !d ;
+        g := i ; )
+  
+      else if i < 32 then (
+        r := f2 !b !c !d ;
+        g := (5*i + 1) mod 16 ; )
       
-    else if i < 48 then (
-      r := f3 !b !c !d ;
-      g := (3*i + 5) mod 16 ; )
+      else if i < 48 then (
+        r := f3 !b !c !d ;
+        g := (3*i + 5) mod 16 ; )
       
-    else (
-      r := f4 !b !c !d ;
-      g := (7*i) mod 16 ; ) ;
+      else (
+        r := f4 !b !c !d ;
+        g := (7*i) mod 16 ; ) ;
   
-    r := Int32.add ( Int32.add (Int32.add !r  !a)  k.(i) ) m.(!g) ;
-    a := !d ;
-    d := !c ;
-    c := !b ;
-    b := Int32.add !b  (rol32 !r s.(i)) ;
+      r := Int32.add ( Int32.add (Int32.add !r  !a)  k.(i) ) m.(!g) ;
+      a := !d ;
+      d := !c ;
+      c := !b ;
+      b := Int32.add !b  (rol32 !r s.(i)) ; 
+      
+      
+    done ;
   
+    h.(0) <- Int32.add h.(0) !a ;
+    h.(1) <- Int32.add h.(1) !b ;
+    h.(2) <- Int32.add h.(2) !c ;
+    h.(3) <- Int32.add h.(3) !d ;
     
-  done ;
+  in
   
-  h.(0) <- Int32.add h.(0) !a ;
-  h.(1) <- Int32.add h.(1) !b ;
-  h.(2) <- Int32.add h.(2) !c ;
-  h.(3) <- Int32.add h.(3) !d ;
+  match m_li with
+  |hd::tl -> 
+      md5 hd;
+      
+      h.(0) <- finish h.(0)  ;
+      h.(1) <- finish h.(1)  ;
+      h.(2) <- finish h.(2)  ;
+      h.(3) <- finish h.(3)  ; 
+      
+      (h.(0),h.(1),h.(2),h.(3))
+  |_-> 
+      (h.(0),h.(1),h.(2),h.(3))
   
-  
-  (h.(0),h.(1),h.(2),h.(3))
+      
 ;;
 
-let test = [|0l;1l;2l;3l;4l;5l;6l;7l;8l;9l;10l;11l;12l;13l;14l;15l|] in Printf.printf "%s\n" (Int128.to_str (cycle_512 test)) ;;
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
 
   
   
